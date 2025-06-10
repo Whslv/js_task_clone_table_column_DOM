44af11220b662810feb28bc637ce46aef26ba747
Whslv <vladon19977@gmail.com>
2025-06-10 15:42:58 -0400

    make build

---
+++ index.4294b034.js
@@ -0,0 +1,2 @@
+!function(e){e.forEach(function(e){Array.from(e.children).forEach(function(e,t){if(1===t){var n=document.createElement(e.tagName.toLowerCase());n.textContent=e.textContent,e.parentNode.insertBefore(n,e.parentNode.lastElementChild)}})})}(document.querySelectorAll("tr"));
+//# sourceMappingURL=index.4294b034.js.map
